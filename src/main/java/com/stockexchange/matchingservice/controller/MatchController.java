2923e7380542f5f51a4dbcc787853d3c2cf282c6
Ianporpino1 <ian.porpino.017@ufrn.edu.br>
2025-10-19 14:49:38 -0300

    graphql

--- src/main/java/com/stockexchange/matchingservice/controller/MatchController.java
+++ src/main/java/com/stockexchange/matchingservice/controller/MatchController.java
@@ -5,27 +5,25 @@ import com.stockexchange.matchingservice.model.dto.MatchResponse;
 import com.stockexchange.matchingservice.model.dto.OrderResponse;
 import com.stockexchange.matchingservice.service.MatchingEngine;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
-
 import java.util.UUID;
 
-@RestController
-@RequestMapping
+@Controller
 public class MatchController {
     
     @Autowired
     private MatchingEngine matchingEngine;
-    
-    @PostMapping("/match")
-    public Mono<ResponseEntity<MatchResponse>> match(@RequestBody Mono<CreateOrderCommand> orderToMatchMono) {
-        return orderToMatchMono
-                .flatMap(orderToMatch -> matchingEngine.matchOrder(orderToMatch))
-                .map(ResponseEntity::ok)
-                .switchIfEmpty(Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build()));
+
+    @MutationMapping
+    public Mono<MatchResponse> matchOrder(@Argument("order") CreateOrderCommand orderToMatch) {
+        return matchingEngine.matchOrder(orderToMatch);
     }
+
 
 
     @GetMapping("/orders/{id}")
