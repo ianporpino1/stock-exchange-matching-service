ae4d5dcccc94f0b458db3a6fceae9e27499b0321
Ianporpino1 <ian.porpino.017@ufrn.edu.br>
2025-12-02 14:46:14 -0300

    ok-rabbit

--- src/main/java/com/stockexchange/matchingservice/handler/MatchHandler.java
+++ src/main/java/com/stockexchange/matchingservice/handler/MatchHandler.java
@@ -1,24 +1,23 @@
 package com.stockexchange.matchingservice.handler;
 
 import com.stockexchange.matchingservice.model.dto.CreateOrderCommand;
+import com.stockexchange.matchingservice.model.dto.OrderResponse;
+import com.stockexchange.matchingservice.model.dto.TradeResponse;
 import com.stockexchange.matchingservice.model.event.OrderCreatedEvent;
 import com.stockexchange.matchingservice.model.event.OrderUpdatedEvent;
 import com.stockexchange.matchingservice.model.event.TradeExecutedEvent;
 import com.stockexchange.matchingservice.service.MatchingEngine;
-import org.springframework.cloud.stream.binder.BinderHeaders;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 import reactor.core.publisher.Flux;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
 @Configuration
 public class MatchHandler {
     private final MatchingEngine matchingEngine;
-    private static final String DESTINATION_HEADER ="spring.cloud.stream.sendto.destination";
 
     public MatchHandler(MatchingEngine matchingEngine) {
         this.matchingEngine = matchingEngine;
@@ -28,29 +27,38 @@ public class MatchHandler {
     public Function<Flux<OrderCreatedEvent>, Flux<Message<?>>> handleMatch() {
         return flux -> flux.concatMap(event ->
                 matchingEngine.matchOrder(CreateOrderCommand.from(event))
-                        .flatMapMany(response -> {
-
-                            List<Message<?>> outputMessages = new ArrayList<>();
-                            if (response.orders() != null) {
-                                response.orders().forEach(order -> {
-                                    var msg = MessageBuilder.withPayload(OrderUpdatedEvent.from(order))
-                                            .setHeader(DESTINATION_HEADER, "orders.updated")
-                                            .build();
-                                    outputMessages.add(msg);
-                                });
-                            }
-
-                            if (response.trades() != null) {
-                                response.trades().forEach(trade -> {
-                                    var msg = MessageBuilder.withPayload(TradeExecutedEvent.from(trade))
-                                            .setHeader(DESTINATION_HEADER, "trades.executed")
-                                            .build();
-                                    outputMessages.add(msg);
-                                });
-                            }
-
-                            return Flux.fromIterable(outputMessages);
-                        })
+                        .flatMapMany(response ->
+                                Flux.merge(
+                                        publishOrders(response.orders()),
+                                        publishTrades(response.trades())
+                                )
+                        )
         );
+    }
+
+    private Flux<Message<?>> publishOrders(List<OrderResponse> orders) {
+        if (orders == null || orders.isEmpty()) {
+            return Flux.empty();
+        }
+        return Flux.fromIterable(orders)
+                .map(order ->
+                        MessageBuilder.withPayload(OrderUpdatedEvent.from(order))
+                                .setHeader("spring.cloud.stream.sendto.destination", "order.events")
+                                .setHeader("eventType", "order.updated")
+                                .build()
+                );
+    }
+
+    private Flux<Message<?>> publishTrades(List<TradeResponse> trades) {
+        if (trades == null || trades.isEmpty()) {
+            return Flux.empty();
+        }
+        return Flux.fromIterable(trades)
+                .map(trade ->
+                        MessageBuilder.withPayload(TradeExecutedEvent.from(trade))
+                                .setHeader("spring.cloud.stream.sendto.destination", "trade.events")
+                                .setHeader("eventType", "trade.executed")
+                                .build()
+                );
     }
 }
